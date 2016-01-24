39222165c374bf19a75d13044e5d0523e2bc67cc
srph <seraphipod@gmail.com>
2016-01-25 01:29:55 +0800

    fix tests to match newer api.

--- test.js
+++ test.js
@@ -3,13 +3,17 @@ var nock = require('nock');
 var base = require('./');
 var axios = require('axios');
 
+var eject = function(interceptor) {
+  axios.interceptors.request.eject(interceptor);
+}
+
 describe('axios-base-url interceptor', function() {
   afterEach(function() {
     nock.cleanAll();
   });
 
   it('should not append the base-url if request url is absolute', function(done) {
-    var undo = base('//yolo.com');
+    var interceptor = base('//yolo.com');
 
     nock('http://swag.com')
       .get('/hehe')
@@ -19,23 +23,23 @@ describe('axios-base-url interceptor', function() {
       .then(function(res) {
         expect(res.data.success).to.be.true;
         expect(res.config.url).to.match(/swag.com\/hehe/);
-        undo();
+        eject(interceptor);
       })
       .then(done, done);
   });
 
   it('should append the base-url otherwise', function(done) {
-    var undo = base('http://persisssists.con');
+    var interceptor = base('http://persisssists.con');
 
     nock('http://persisssists.con')
       .get('/hehe')
       .reply(200, { success: true });
 
-    axios.get('http://persisssists.con/hehe')
+    axios.get('hehe')
       .then(function(res) {
         expect(res.data.success).to.be.true;
         expect(res.config.url).to.match(/persisssists.con\/hehe/);
-        undo();
+        eject(interceptor);
       })
       .then(done, done);
   });
